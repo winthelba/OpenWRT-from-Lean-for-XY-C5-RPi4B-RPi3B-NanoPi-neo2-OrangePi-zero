48092c05a3ff1ff7fb9dae50bf5caaa3aeba6b95
winthelba <45763470+winthelba@users.noreply.github.com>
2020-09-17 12:13:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages
+sed -i '$a src-git small https://github.com/kenzok8/small
