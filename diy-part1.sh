87f971ab3504ed4306cc46f63c74ac68dc6f4d2e
winthelba <45763470+winthelba@users.noreply.github.com>
2020-09-17 12:18:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,16 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
+# Add a feed source by kenzok8/openwrt-packages
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages
 sed -i '$a src-git small https://github.com/kenzok8/small
+
+# Add apps
+#cd lede/package/lean/ 
+git clone https://github.com/rufengsuixing/luci-app-onliner
+git clone https://github.com/kang-mk/luci-app-smartinfo
+git clone https://github.com/jerrykuku/lua-maxminddb.git
+git clone https://github.com/jerrykuku/luci-app-vssr.git
+git clone https://github.com/destan19/OpenAppFilter.git
+git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk
+git clone https://github.com/BoringCat/luci-app-mentohust.git
