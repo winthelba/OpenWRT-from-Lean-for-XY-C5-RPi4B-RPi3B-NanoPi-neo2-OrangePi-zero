d771e47db69971d777e70d5c6c7a6bc38ba666f5
winthelba <45763470+winthelba@users.noreply.github.com>
2020-09-20 08:46:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,8 +17,8 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Add a feed source by kenzok8/openwrt-packages
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages
-sed -i '$a src-git small https://github.com/kenzok8/small
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add apps to profile-lean
 pushd package/lean
